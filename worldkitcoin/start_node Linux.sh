51566af89cf7ebefc13d52b810249694991a039d
Mik0s <q58693@yandex.ru>
2026-06-23 17:09:57 +0700

    Delete worldkitcoin directory

--- worldkitcoin/start_node Linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-pip install -r requirements.txt
-echo "Starting node..."
-python3 main.py
--- worldkitcoin/start_node Windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title WorldKitCoin Node
-echo Installing dependencies...
-pip install -r requirements.txt > nul
-echo Starting node...
-python main.py
-pause
